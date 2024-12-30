30a5398138789018823c5fe583be315e62848e22
sarahamelo <sarah.alves.melo@hotmail.com>
2024-12-30 09:47:31 -0300

    fix

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,3 +1,4 @@
+from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Category
@@ -5,6 +6,7 @@ from product.serializers import CategorySerializer
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
+    renderer_classes = [JSONRenderer]
 
     def get_queryset(self):
         return Category.objects.all()
